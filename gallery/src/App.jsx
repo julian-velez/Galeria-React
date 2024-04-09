cfc9e1641ad0ad2f817db9919ff00bbe3fefb02b
julian <juliansolo7189@gmail.com>
2024-04-09 11:42:46 -0500

    Galeria-React

---
+++ gallery/src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import img1 from "../img/uno.jpg";
+import img2 from "../img/dos.jpg";
+import img3 from '../img/tres.jpg';
+import img4 from '../img/cuatro.jpg';
+import img5 from '../img/cinco.jpg';
+import img6 from '../img/seis.jpg';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Gallery React</h1>
+        <img src={img1} alt="" />
+        <img src={img2} alt="" />
+        <img src={img3} alt="" />
+        <img src={img4} alt="" />
+        <img src={img5} alt="" />
+        <img src={img6} alt="" />
+      </div>
+    </>
+  );
+  {
+  }
+}
+
+export default App;
